b79374907a2825ddc00af925587374c3d5fd4a86
Jesus Alejandro Pinto Montoya <jpinto@grupogexo.com>
2020-05-17 14:45:45 -0500

    Updates

---
+++ precache-manifest.b2e818a7e053a564d6774ed93338c470.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f671f0e2c90607db3cec215c9f012013",
+    "url": "/alejomontoya/three-easy-react-projects/index.html"
+  },
+  {
+    "revision": "7281bce3350e383f3110",
+    "url": "/alejomontoya/three-easy-react-projects/static/css/main.778f9016.chunk.css"
+  },
+  {
+    "revision": "18372eb817ea42497b6e",
+    "url": "/alejomontoya/three-easy-react-projects/static/js/2.220beef5.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/alejomontoya/three-easy-react-projects/static/js/2.220beef5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7281bce3350e383f3110",
+    "url": "/alejomontoya/three-easy-react-projects/static/js/main.4c966815.chunk.js"
+  },
+  {
+    "revision": "db750c18c5045c0fab1d",
+    "url": "/alejomontoya/three-easy-react-projects/static/js/runtime-main.041f75f0.js"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/alejomontoya/three-easy-react-projects/precache-manifest.b2e818a7e053a564d6774ed93338c470.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/alejomontoya/three-easy-react-projects/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
+});
